bfe15a8012c3e88a06491097e82c9faf7b6f48ee
thiagola92 <thiagola92@gmail.com>
2022-10-06 07:45:08 -0300

    Configure machine and install pypiserver

--- main.tf
+++ main.tf
@@ -4,6 +4,11 @@ terraform {
       source  = "kreuzwerker/docker"
       version = "~> 2.20.0"
     }
+
+    linux = {
+      source= "TelkomIndonesia/linux"
+      version = "~> 0.7.0"
+    }
   }
 }
 
@@ -17,7 +22,18 @@ variable "machine_user" {
   sensitive = true
 }
 
+variable "machine_pass" {
+  type      = string
+  sensitive = true
+}
+
 provider "docker" {
   host     = "ssh://${var.machine_user}@${var.machine_ip}:22"
   ssh_opts = ["-o", "StrictHostKeyChecking=no", "-o", "UserKnownHostsFile=/dev/null"]
+}
+
+provider "linux" {
+  host = var.machine_ip
+  user = var.machine_user
+  password = var.machine_pass
 }
