93682222daf97baa6fa0b8184a10dd3c1cc47e6e
thiagola92 <thiagola92@gmail.com>
2022-10-13 07:32:35 -0300

    Remove machine_user and machine_pass, it's not necessary after copying your id to machine

--- main.tf
+++ main.tf
@@ -17,23 +17,12 @@ variable "machine_ip" {
   sensitive = true
 }
 
-variable "machine_user" {
-  type      = string
-  sensitive = true
-}
-
-variable "machine_pass" {
-  type      = string
-  sensitive = true
-}
-
 provider "docker" {
-  host     = "ssh://${var.machine_user}@${var.machine_ip}:22"
+  host     = "ssh://${var.machine_ip}:22"
   ssh_opts = ["-o", "StrictHostKeyChecking=no", "-o", "UserKnownHostsFile=/dev/null"]
 }
 
 provider "linux" {
   host     = var.machine_ip
   user     = var.machine_user
-  password = var.machine_pass
 }
